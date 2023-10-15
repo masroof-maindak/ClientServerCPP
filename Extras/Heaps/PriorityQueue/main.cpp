3a6d6e30b2a2bfc8b2c5eff31e7c3b628e701468
masroof-maindak <mujtaba.asim.amin@gmail.com>
2023-10-15 23:18:34 +0500

    task 4 done. 3/5 left

--- Extras/Heaps/PriorityQueue/main.cpp
+++ Extras/Heaps/PriorityQueue/main.cpp
@@ -2,6 +2,16 @@
 #include "priorityQueue.h"
 
 int main () {
+    PriorityQueue<int> Q;
+
+    Q.push(20);
+    Q.push(10);
+    Q.push(30);
+    Q.push(40);
+    Q.pop();
+    std::cout << "Front: " << Q.front() << std::endl;
+    Q.push(35);
+    std::cout << "Front: " << Q.front() << std::endl;
 
     return 0;
 }
--- Extras/Heaps/PriorityQueue/priorityQueue.h
+++ Extras/Heaps/PriorityQueue/priorityQueue.h
@@ -8,15 +8,16 @@ template <class T>
 class PriorityQueue 
 {
     maxHeap<T> PQ;
-    int _size;
 public:
-    PriorityQueue(); // You know guys...
-    void push(int data); // insert element into queue
-    void pop(); // remove highest key value element
-    T front(); // return the element with highest key value
-    bool empty() {return _size == 0;} // tell whether queue is empty or not
-    int size() {return _size;} // tell the size of the queue
-    ~PriorityQueue() {delete PQ;} // do whatever is necessary...  
+    PriorityQueue() {}; // You know guys...
+
+    void push(int data) {PQ.insert(data);}; // insert element into queue
+    void pop() {PQ.erase(front());} // remove highest key value element
+
+    T front() {return PQ.getMax();} // return the element with highest key value
+    bool empty () {return PQ.empty();} // tell whether queue is empty or not
+    int size () {return PQ.size();} // tell the size of the queue
+    ~PriorityQueue() {} // do whatever is necessary...  
 };
 
 #endif
